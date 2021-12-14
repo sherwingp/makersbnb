0c89b9ffefd9662be4d042849371a7ac80ce9150
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-14 16:43:06 +0000

    Write test for email validation

--- spec/features/registrations/sign_up_spec.rb
+++ spec/features/registrations/sign_up_spec.rb
@@ -1,8 +1,28 @@
 feature 'Registration' do
+
   context 'Signing up' do
     scenario 'shows indication of sign up' do
-      sign_up
+      visit '/'
+      click_button 'Create New Account'
+      fill_in('first_name', with: 'Sherwin')
+      fill_in('last_name', with: 'Test')
+      fill_in('email', with: 'test@test.com')
+      fill_in('password', with: 'password')
+      click_button('Sign Up')
       expect(page).to have_content("Sherwin")
+    end
+
+    context 'Shows an error' do
+      scenario 'when an invalid email is given' do
+        visit '/'
+        click_button 'Create New Account'
+        fill_in('first_name', with: 'Sherwin')
+        fill_in('last_name', with: 'Test')
+        fill_in('email', with: 'test@test')
+        fill_in('password', with: 'password')
+        click_button('Sign Up')
+        expect(page).to have_content("Invalid email")
+      end
     end
   end
 end
