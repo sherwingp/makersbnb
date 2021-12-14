3e2fdfbb1360ad6ce9c4ed458e09518af88eb80e
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-14 14:43:38 +0000

    Write feature tests for invalid logins

--- spec/features/authentication/log_in_spec.rb
+++ spec/features/authentication/log_in_spec.rb
@@ -4,4 +4,20 @@ feature 'logging in' do
     log_in
     expect(page).to have_content "Sherwin"
   end
+
+  context 'with an invalid email' do
+    scenario 'gives an indication of logging in' do
+      sign_up
+      log_in
+      expect(page).to have_content "Invalid email or pasword"
+    end
+  end
+
+  context 'with an invalid password' do
+    scenario 'gives an indication of logging in' do
+      sign_up
+      log_in
+      expect(page).to have_content "Invalid email or password"
+    end
+  end
 end
