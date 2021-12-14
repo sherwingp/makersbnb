a4a91f4b5a58659ddf86869692dec22d14d433c8
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-14 11:57:46 +0000

    Write unit tests for log in authentication

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -14,4 +14,26 @@ describe User do
       expect(user.email).to eq 'test@test.com'
     end
   end
+
+  describe '.authenticate' do
+    before(:each) do
+      @user = User.create(
+        first_name: 'Sherwin', 
+        last_name: 'Test', 
+        email: 'test@test.com', 
+        password: 'test'
+      )
+    end
+    it 'returns a user instance when given correct details' do
+      expect(User.authenticate(email: @user.email, password: @user.password)).to be_a User
+    end
+
+    it 'returns nil when given a wrong email' do
+      expect(User.authenticate(email: 'wrong email', password: @user.password)).to eq nil
+    end
+
+    it 'returns nil when given a wrong password' do
+      expect(User.authenticate(email: @user.email, password: 'wrong password')).to eq nil
+    end
+  end
 end
