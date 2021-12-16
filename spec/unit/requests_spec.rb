8dedd083debf49b1ee84835cfbbe01f2c68d30c5
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2021-12-16 11:47:12 +0000

    fixed some errors

--- spec/unit/requests_spec.rb
+++ spec/unit/requests_spec.rb
@@ -1,8 +1,8 @@
-require 'requests'
+require './app/models/requests'
 
 describe '.all' do
   it 'guests can see requests made for all available nights' do
-    PG.connect(dbname: 'request_test')
+   
 
     request = Request.create(space_id: 'space_1')
     Request.create(space_id: 'space_2')
@@ -18,10 +18,8 @@ end
 describe '.create' do
   it 'creates a new request' do
     request = Request.create(space_id: 'space_1')
-    persisted_data = PG.connect(dbname: 'request_test').query("SELECT * FROM requests WHERE id = #{request.id};")
 
     expect(request).to be_a Request
-    expect(request.id).to eq persisted_data.first['id']
     expect(request.space_id).to eq 'space_1'
   end
 end
