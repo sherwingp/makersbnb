701a5d35598423f02bf6c29affd8bafee07e1e96
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-16 15:38:59 +0000

    Implement list space with host integration

--- app/models/spaces.rb
+++ app/models/spaces.rb
@@ -4,30 +4,32 @@ require './lib/database_connection'
 require 'pg'
 
 class Spaces
-  attr_reader :location, :price, :host_id, :id
+  attr_reader :location, :price, :host_id, :id, :description
 
   
 
-  def initialize(location:, price:, host_id:, id:)
+  def initialize(location:, price:, host_id:, id:, description:)
     @location = location
     @price = price
     @host_id = host_id
     @id = id
+    @description = description
   end
 
-  def self.create(location:, price:, host_id:)
+  def self.create(location:, price:, description:, host_id:)
     result = DatabaseConnection.query(
-      'INSERT INTO spaces (location, price, host_id) VALUES ($1, $2, $3) RETURNING id, location, price, host_id;', [location,
-                                                                                                                    price, host_id]
+      'INSERT INTO spaces (location, price, description, host_id) VALUES ($1, $2, $3, $4) RETURNING id, location, price, description, host_id;', [location,
+                                                                                                                    price, description, host_id]
     )
-    Spaces.new(location: result[0]['location'], price: result[0]['price'], host_id: result[0]['host_id'],
-               id: result[0]['id'])
+    Spaces.new(location: result[0]['location'], price: result[0]['price'], description: result[0]['description'],
+               id: result[0]['id'], host_id: result[0]['host_id'])
   end
 
   def self.list
     result = DatabaseConnection.query('SELECT * FROM spaces;')
     result.map do |space|
-      Spaces.new(location: space['location'], price: space['price'], host_id: space['host_id'], id: space['id'])
+      Spaces.new(location: result[0]['location'], price: result[0]['price'], description: result[0]['description'],
+        id: result[0]['id'], host_id: result[0]['host_id'])
     end
   end
 end
